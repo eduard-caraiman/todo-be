7fd9980b321d5872455b11aaf1928a0f2593d8fd
Eduard <16525969+eduard-caraiman@users.noreply.github.com>
2026-09-22 10:11:23 +0300

    Unit tests - added

---
+++ tests/Todo.Tests/UpdateTodoRequestTests.cs
@@ -0,0 +1,32 @@
+﻿namespace todo_be.Tests;
+
+using todo_be.Todos;
+
+public class UpdateTodoRequestTests
+{
+    [Fact]
+    public void ApplyTo_UpdatesEditableProperties()
+    {
+     
+        var todo = new Todo
+        {
+            Title = "Titlu vechi",
+            Description = "Descriere veche",
+            IsCompleted = false
+        };
+        var request = new UpdateTodoRequest
+        {
+            Title = "Titlu nou",
+            Description = "Descriere noua",
+            IsCompleted = true
+        };
+
+      
+        request.ApplyTo(todo);
+
+      
+        Assert.Equal("Titlu nou", todo.Title);
+        Assert.Equal("Descriere noua", todo.Description);
+        Assert.True(todo.IsCompleted);
+    }
+}
